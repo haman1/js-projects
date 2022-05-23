aca5aa23cf7dc48011fb9cc1f4558c3a6cc8479a
ombasa <ombasaherman@gmail.com>
2022-05-24 00:18:34 +0300

    highscores implementation

--- number-guesser/app.js
+++ number-guesser/app.js
@@ -1,9 +1,8 @@
 'use strict';
 
-let secretNumber =  Math.trunc(Math.random() * 20)+1;
-
-
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -16,8 +15,13 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent = '🎉Correct Number'
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor= '#60b347';
-
         document.querySelector('number').style.width = '30rem';
+
+       if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+          }
+      
     } else if (guess > secretNumber) {
 
         if( score > 1) {
